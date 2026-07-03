0425581d424f82fc2d285e148efe327867b62ea9
HUIYWU <13970431201@139.com>
2026-07-03 08:09:14 +0800

    import

--- java/lsp/src/main/java/com/tom/rv2ide/lsp/java/utils/EditHelper.java
+++ java/lsp/src/main/java/com/tom/rv2ide/lsp/java/utils/EditHelper.java
@@ -24,6 +24,7 @@ import com.tom.rv2ide.lsp.models.TextEdit;
 import com.tom.rv2ide.models.Position;
 import com.tom.rv2ide.models.Range;
 import com.tom.rv2ide.projects.util.StringSearch;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Collections;
